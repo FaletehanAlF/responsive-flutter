025357c872a785285208424f2cfd32a6b2338e29
FaletehanAlF <faletehanalfarabi09@gmail.com>
2026-09-22 21:07:25 +0700

    update code

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,96 +1,133 @@
 import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+
 import '../models/post.dart';
+import '../models/category.dart';
 
 class ApiService {
-  final String baseUrl = 'http://localhost:8000';
-
-  Future<List<Post>> getPosts() async {
-    final response = await http.get(
-      Uri.parse('$baseUrl/posts'),
-    );
+  Future<List<Category>> getCategories() async {
+    final response = await http.get(Uri.parse('$baseUrl/categories'));
 
     if (response.statusCode == 200) {
       final data = jsonDecode(response.body);
 
       return (data['data'] as List)
-          .map((item) => Post.fromJson(item))
+          .map((item) => Category.fromJson(item))
           .toList();
+    } else {
+      throw Exception('Gagal mengambil kategori');
+    }
+  }
+
+  Future<void> addCategory(String name) async {
+    final response = await http.post(
+      Uri.parse('$baseUrl/categories'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({'name': name}),
+    );
+
+    if (response.statusCode != 201) {
+      throw Exception('Gagal menambahkan kategori');
+    }
+  }
+
+  Future<void> updateCategory(int id, String name) async {
+    final response = await http.put(
+      Uri.parse('$baseUrl/categories/$id'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({'name': name}),
+    );
+
+    if (response.statusCode != 200) {
+      throw Exception('Gagal mengubah kategori');
+    }
+  }
+
+  Future<void> deleteCategory(int id) async {
+    final response = await http.delete(Uri.parse('$baseUrl/categories/$id'));
+
+    if (response.statusCode != 200) {
+      throw Exception('Gagal menghapus kategori');
+    }
+  }
+
+  final String baseUrl = 'http://localhost:8000';
+
+  Future<List<Post>> getPosts() async {
+    final response = await http.get(Uri.parse('$baseUrl/posts'));
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+
+      return (data['data'] as List).map((item) => Post.fromJson(item)).toList();
     } else {
       throw Exception('Gagal mengambil artikel');
     }
   }
 
   Future<Post> getPostById(int id) async {
-  final response = await http.get(
-    Uri.parse('$baseUrl/posts/$id'),
-  );
+    final response = await http.get(Uri.parse('$baseUrl/posts/$id'));
 
-  if (response.statusCode == 200) {
-    final data = jsonDecode(response.body);
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
 
-    return Post.fromJson(data['data']);
-  } else {
-    throw Exception('Gagal mengambil detail artikel');
+      return Post.fromJson(data['data']);
+    } else {
+      throw Exception('Gagal mengambil detail artikel');
+    }
   }
-}
 
-Future<void> addPost({
-  required String title,
-  required String content,
-  required int categoryId,
-  String? image,
-}) async {
-  final response = await http.post(
-    Uri.parse('$baseUrl/posts'),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: jsonEncode({
-      'title': title,
-      'content': content,
-      'image': image,
-      'category_id': categoryId,
-    }),
-  );
+  Future<void> addPost({
+    required String title,
+    required String content,
+    required int categoryId,
+    String? image,
+  }) async {
+    final response = await http.post(
+      Uri.parse('$baseUrl/posts'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({
+        'title': title,
+        'content': content,
+        'image': image,
+        'category_id': categoryId,
+      }),
+    );
 
-  if (response.statusCode != 201) {
-    throw Exception('Gagal menambahkan artikel');
+    if (response.statusCode != 201) {
+      throw Exception('Gagal menambahkan artikel');
+    }
   }
-}
 
-Future<void> updatePost({
-  required int id,
-  required String title,
-  required String content,
-  required int categoryId,
-  String? image,
-}) async {
-  final response = await http.put(
-    Uri.parse('$baseUrl/posts/$id'),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: jsonEncode({
-      'title': title,
-      'content': content,
-      'image': image,
-      'category_id': categoryId,
-    }),
-  );
+  Future<void> updatePost({
+    required int id,
+    required String title,
+    required String content,
+    required int categoryId,
+    String? image,
+  }) async {
+    final response = await http.put(
+      Uri.parse('$baseUrl/posts/$id'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({
+        'title': title,
+        'content': content,
+        'image': image,
+        'category_id': categoryId,
+      }),
+    );
 
-  if (response.statusCode != 200) {
-    throw Exception('Gagal mengubah artikel');
+    if (response.statusCode != 200) {
+      throw Exception('Gagal mengubah artikel');
+    }
   }
-}
 
-Future<void> deletePost(int id) async {
-  final response = await http.delete(
-    Uri.parse('$baseUrl/posts/$id'),
-  );
+  Future<void> deletePost(int id) async {
+    final response = await http.delete(Uri.parse('$baseUrl/posts/$id'));
 
-  if (response.statusCode != 200) {
-    throw Exception('Gagal menghapus artikel');
+    if (response.statusCode != 200) {
+      throw Exception('Gagal menghapus artikel');
+    }
   }
-}
 }
