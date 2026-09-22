3c4d80857db47a352e6da46840c0332583404944
FaletehanAlF <faletehanalfarabi09@gmail.com>
2026-09-22 20:08:09 +0700

    add file

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import '../services/api_service.dart';
+import '../models/post.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final ApiService apiService = ApiService();
+
+  late Future<List<Post>> posts;
+
+  @override
+  void initState() {
+    super.initState();
+    posts = apiService.getPosts();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('NARATA'),
+      ),
+      body: FutureBuilder<List<Post>>(
+        future: posts,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          if (snapshot.hasError) {
+            return Center(
+              child: Text('Error: ${snapshot.error}'),
+            );
+          }
+
+          final data = snapshot.data ?? [];
+
+          if (data.isEmpty) {
+            return const Center(
+              child: Text('Belum ada artikel'),
+            );
+          }
+
+          return ListView.builder(
+            itemCount: data.length,
+            itemBuilder: (context, index) {
+              final post = data[index];
+
+              return ListTile(
+                title: Text(post.title),
+                subtitle: Text(post.categoryName),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
