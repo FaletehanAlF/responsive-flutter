094e37457b4ffef48e8be3dfca1df583d7bd99e2
FaletehanAlF <faletehanalfarabi09@gmail.com>
2026-09-22 09:57:49 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Percobaan Flexible'),
+        ),
+        body: Column(
+          children: [
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.amber,
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.cyan,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
