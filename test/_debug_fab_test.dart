2d54aa559fb027c59b9c8ab7af3c4ce45a04c117
FaletehanAlF <faletehanalfarabi09@gmail.com>
2026-09-26 01:14:26 +0700

    update feature

---
+++ test/_debug_fab_test.dart
@@ -0,0 +1,25 @@
+﻿import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart' as http;
+import 'package:http/testing.dart';
+import 'package:responsive_ui/pages/category_page.dart';
+import 'package:responsive_ui/services/api_service.dart';
+import 'dart:convert';
+
+void main() {
+  testWidgets('fab', (tester) async {
+    final client = MockClient((r) async => http.Response(
+        jsonEncode({'success': true, 'data': <Map<String, dynamic>>[]}), 200));
+    await tester.pumpWidget(MaterialApp(
+      home: CategoryPage(apiService: ApiService(baseUrl: 'http://t', client: client)),
+    ));
+    await tester.pump();
+    await tester.pump(const Duration(milliseconds: 200));
+    debugPrint('FAB count: ${find.byType(FloatingActionButton).evaluate().length}');
+    await tester.tap(find.byType(FloatingActionButton));
+    await tester.pump();
+    await tester.pump(const Duration(milliseconds: 500));
+    debugPrint('Dialog text: ${find.text('Tambah Kategori').evaluate().length}');
+    debugPrint('AlertDialog count: ${find.byType(AlertDialog).evaluate().length}');
+  });
+}
